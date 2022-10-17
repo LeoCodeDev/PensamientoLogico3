54824367ea93e718d675a711252076159db601a9
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-10-17 10:33:01 -0500

    Inicio de lenguaje python

---
+++ python/main.py
@@ -0,0 +1,11 @@
+#Calculadora basica con python
+
+valor1 = 7
+valor2 = 6
+
+resultadoSuma = valor1 + valor2
+resultadoResta = valor2 - valor1
+resultadoMultiplicaion = valor1 * valor2
+resultadoDivision = valor2 / valor1
+
+print(f'La suma es:{resultadoSuma}, La Resta es: {resultadoResta}, La multiplicacion es: {resultadoMultiplicaion} y la division es {resultadoDivision}')
